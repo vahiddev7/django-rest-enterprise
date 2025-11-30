311b1ad66600d580682014f438dfb9ad077c8b1d
VahidZareCE <vahid.zare.ce@gmail.com>
2025-11-30 12:11:01 +0330

    add Basemodel

--- {{cookiecutter.project_name}}/modules/users/infrastructure/models.py
+++ {{cookiecutter.project_name}}/modules/users/infrastructure/models.py
@@ -1,5 +1,6 @@
 # --- models.py ---
 from django.contrib.auth.models import AbstractUser
+from core.models import BaseModel
 
-class User(AbstractUser):
+class User(AbstractUser, BaseModel):
     pass
